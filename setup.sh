7ba614225d1ace63fadb6b219eec5951f79b6157
Keane_Tan <chin.lung.t93@gmail.com>
2023-04-12 14:56:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc700
 eval `scramv1 project CMSSW CMSSW_10_2_13`
 cd CMSSW_10_2_13/src/
-eval `scramv1 runtime -sh`
+git clone git@github.com:Keane-Tan/getLimits.git
+cd getLimits
 git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 cd HiggsAnalysis/CombinedLimit
 source env_standalone.sh 
 make -j 8; make
 cd ../..
-git clone git@github.com:Keane-Tan/getLimits.git
